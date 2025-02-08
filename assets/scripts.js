40b6a1bfca20f6531124f77935cae4bf6745c7d8
Amir-Afkir <afkir202@gmail.com>
2025-02-08 04:20:32 +0100

    new20

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const gallery = document.querySelector(".gallery");
-    
-    if (gallery && typeof gallery.mauGallery === "function") {
-        gallery.mauGallery({
-            columns: {
-                xs: 1,
-                sm: 2,
-                md: 3,
-                lg: 3,
-                xl: 3
-            },
-            lightBox: true,
-            lightboxId: 'myAwesomeLightbox',
-            showTags: true,
-            tagsPosition: 'top'
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    if (typeof MauGallery === "function") {
+      new MauGallery(".gallery", {
+        columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+      });
     } else {
-        console.error("Erreur : la fonction mauGallery() n'est pas disponible.");
+      console.error("Erreur : la fonction MauGallery() n'est pas dispo.");
     }
-});
+  });
+  
